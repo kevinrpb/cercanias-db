b1275cf84352c4d37210c53f16fc1edf867f7e90
Kevin Romero Peces-Barba <kevinrpb@hotmail.com>
2022-03-21 12:12:13 +0100

    Improve the form UI and add date

--- components/form.js
+++ components/form.js
@@ -6,22 +6,24 @@ import { getFirestore, collection } from 'firebase/firestore'
 import { useCollectionData } from 'react-firebase-hooks/firestore'
 import { useForm } from 'react-hook-form'
 
-const Select = ({ id, loading, elements, register }) => (
+const Select = ({ id, title, loading, elements, register }) => (<>
+  <label for={id}>{title}</label>
   <select
     id={id}
+    name={id}
     {...register(id, { required: '' })}
   >
     {loading &&
-      <option key='loading' value='loading'>Loading...</option>
+      <option key="loading" value="loading">Loading...</option>
     }
     {elements && elements.map((element) => (
       <option key={element.id} value={element.id}>{element.name}</option>
     ))}
   </select>
-)
+</>)
 
 const Form = (props) => {
-  const { onSubmit, onError } = props
+  const { onSubmit, onError, ...otherProps } = props
 
   const app = createFirebaseApp()
   const db = getFirestore(app)
@@ -38,9 +40,15 @@ const Form = (props) => {
     formState,
   } = useForm()
 
+  const TODAY = new Date()
+  const minDate = TODAY.toISOString().split('T')[0]
+  const TWO_WEEKS = new Date(TODAY.getTime() + (14*24*60*60*1000))
+  const maxDate = TWO_WEEKS.toISOString().split('T')[0]
+
   const ZoneSelect = () => (
     <Select
-      id='zoneID'
+      id="zoneID"
+      title="Zone"
       loading={zonesLoading}
       elements={zones}
       register={register}
@@ -49,7 +57,8 @@ const Form = (props) => {
 
   const OriginSelect = () => (
     <Select
-      id='originID'
+      id="originID"
+      title="Origin"
       loading={stationsLoading}
       elements={stations}
       register={register}
@@ -58,18 +67,32 @@ const Form = (props) => {
 
   const DestinationSelect = () => (
     <Select
-      id='destinationID'
+      id="destinationID"
+      title="Destination"
       loading={stationsLoading}
       elements={stations}
       register={register}
     />
   )
 
+  const DateInput = () => (<>
+    <label for="dateString">Date</label>
+    <input
+      id="dateString"
+      name="dateString"
+      type="date"
+      min={minDate}
+      max={maxDate}
+      {...register("dateString", { required: '' })}
+    />
+  </>)
+
   return (
-    <form onSubmit={handleSubmit(onSubmit, onError)} {...props}>
+    <form onSubmit={handleSubmit(onSubmit, onError)} {...otherProps}>
       <ZoneSelect />
       <OriginSelect />
       <DestinationSelect />
+      <DateInput />
 
       <button type='submit'>Submit</button>
     </form>
